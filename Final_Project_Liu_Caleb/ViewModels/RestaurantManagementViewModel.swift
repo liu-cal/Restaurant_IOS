3da42e0f226844bb6ad6a76aad047f80d3fa6a43
Caleb Liu <liucaleb8@gmail.com>
2023-10-23 09:56:39 -0400

    Implemented few ViewModel functions

---
+++ Final_Project_Liu_Caleb/ViewModels/RestaurantManagementViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  MealNumHandler.swift
+//  Final_Project_Liu_Caleb
+//
+//  Created by macuser on 2023-10-18.
+//
+
+import AVKit
+import SwiftUI
+
+final class RestaurantManagementViewModel: ObservableObject{
+    //initialize meals in database
+    func initializeMeals(){
+        
+    }
+    
+//    //for existing user
+//    func login(username: String, password: String){
+//
+//    }
+//
+//    //for new user
+//    func signUp(firstName: String, lastName: String, username: String, password: String){
+//
+//    }
+    
+    //goes to the Checkout page and passes it the meals that were selected
+    func checkoutMeals(){
+        
+    }
+    
+    //goes to the Confirmation menu and passes it the order information to display it back to user
+    func payOrder(){
+        
+    }
+    
+    //calculates the total cost of the order
+    func calculateTotalOrderCost(){
+        
+    }
+    
+//    //cancel the order and goes back to the landing page
+//    func cancelOrder(){
+//
+//    }
+    
+//    //goes back to the landing page
+//    func backToMenu(){
+//
+//    }
+//
+//    //goes back to the meals page
+//    func keepShopping(){
+//
+//    }
+}
